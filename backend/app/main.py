42365f6621e21f0246bf9560b9428a2aa4964972
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-09-07 14:29:54 +0900

    feat: connect backend to local database

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+from typing import List
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+from database import SessionLocal, engine
+import uvicorn
 
+# models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app=app)
