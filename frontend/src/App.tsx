c25c5a22875049c8921ee77373a8447b7a090d7d
Hiro-Kanetake <hiro_k@live.com>
2022-09-07 14:32:11 +0900

    style: edit privacy policy text

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/organisms/Header"
+import Footer from "./components/organisms/Footer"
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <header className="App-header">
           🚧 melBee 開発中・・・ 🚧　<br />
           完成までしばらくお待ちください
       </header>
+      <Footer></Footer>
     </div>
   );
 }
