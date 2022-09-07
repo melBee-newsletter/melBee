7268c91d6a27b356a2dd98af2bad1ebe3376fd21
kugaitomomi <tottomo7@gmail.com>
2022-09-07 15:18:35 +0900

    chore create index

--- frontend/src/components/templates/Login.tsx
+++ frontend/src/components/templates/Login.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from "react";
 
-function Login(){
-
+function Login() {
+  return (
+    <div>
+      <h1>ログイン</h1>
+      <form>
+        <label htmlFor="mail">メールアドレス</label>
+        <input
+          type="mail"
+          name=""
+          placeholder="youremail@example.com"
+          id="mail"
+        />
+        <label htmlFor="">パスワード</label>
+        <input type="password" name="" id="パスワード" />
+        <input type="submit" value="ログインする"></input>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
--- frontend/src/components/templates/Signup.tsx
+++ frontend/src/components/templates/Signup.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from "react";
 
-function Signup(){
-
+function Signup() {
+  return (
+    <div>
+      <h1>新規登録</h1>
+      <form>
+        <label htmlFor="mail_signup">新規登録</label>
+        <input
+          type="mail"
+          name=""
+          placeholder="youremail@example.com"
+          id="mail_signup"
+        />
+        <label htmlFor="password_signup">パスワード</label>
+        <input type="password" name="" id="password_signup" />
+        <input type="submit" value="新規登録する"></input>
+      </form>
+    </div>
+  );
 }
 
 export default Signup;
