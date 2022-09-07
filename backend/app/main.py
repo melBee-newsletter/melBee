b6c10d07e694e00f54bc7817f68f3155598cf70c
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-09-07 15:13:36 +0900

    feat: modularize the database functions

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 from typing import List
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
-from database import SessionLocal, engine
+from database import crud, models, schemas
+from database.database import SessionLocal, engine
+# from backend.database.database import SessionLocal, engine
 import uvicorn
 
-# models.Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 # Dependency
@@ -20,6 +22,12 @@ def get_db():
 async def root():
     return {"message": "Hello World"}
 
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
 
 if __name__ == '__main__':
     uvicorn.run(app=app)
