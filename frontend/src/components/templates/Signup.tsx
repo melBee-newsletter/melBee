0b55fdb2fe2aa94fd2b2fb748c258a0df277a42a
kugaitomomi <tottomo7@gmail.com>
2022-09-07 14:17:48 +0900

    chore: create signup

--- frontend/src/components/templates/Signup.tsx
+++ frontend/src/components/templates/Signup.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from "react";
 
-function Signup(){
+function Signup() {
+  return (
+    <div>
+      <h1>新規登録</h1>
 
+      <form>
+        <label htmlFor="mail">メールアドレス</label>
+        <input type="mail" name="" id="mail" />
+        <label htmlFor="">パスワード</label>
+        <input type="password" name="" id="パスワード" />
+        <input type="submit" value="登録する"></input>
+      </form>
+    </div>
+  );
 }
 
 export default Signup;
