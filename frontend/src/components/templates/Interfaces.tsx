25d5ec0a515aba0869d345a8a074b8645117d4a0
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-09-08 14:59:55 +0900

    feat: get user sign up with their email/password

--- frontend/src/components/templates/Interfaces.tsx
+++ frontend/src/components/templates/Interfaces.tsx
@@ -2,3 +2,8 @@ export interface LogInForm extends HTMLElement {
   email?: HTMLInputElement;
   password?: HTMLInputElement;
 }
+
+export interface SignUpForm extends HTMLElement {
+  email_signup?: HTMLInputElement;
+  password_signup?: HTMLInputElement;
+}
