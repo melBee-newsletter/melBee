2ec4a841c3ac1a42d059749550526e12a788f27c
Hiro-Kanetake <hiro_k@live.com>
2022-09-09 13:38:30 +0900

    chore: include temporary contents in empty components

--- frontend/src/components/templates/ReceiverSelect.tsx
+++ frontend/src/components/templates/ReceiverSelect.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 function ReceiverSelect(){
+    return (
+        <div style={{backgroundColor: "yellow"}}>
+            <h3>送信先をお選びください</h3>
+            <br />
+            🚧 melBee 開発中・・・ 🚧
+            <br />
+            完成までしばらくお待ちください
+            <br />
+        </div>
+    );
 
-}
+};
 
 export default ReceiverSelect;
