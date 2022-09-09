c9e9fdc934b719838f7f69caacf329c876fde3fa
Hiro-Kanetake <hiro_k@live.com>
2022-09-09 16:48:14 +0900

    feat: make HTML to display

--- frontend/src/components/templates/PreviewBox.tsx
+++ frontend/src/components/templates/PreviewBox.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 
-function PreviewBox(){
+type Props ={
+    editedFile: string;
+};
+
+const PreviewBox: React.FC<Props> = ({editedFile}) => {
     return (
         <div style={{backgroundColor: "yellow"}}>
             <h3>プレビュー</h3>
             <h5>内容をご確認ください</h5>
             <br />
-            🚧 melBee 開発中・・・ 🚧
-            <br />
-            完成までしばらくお待ちください
-            <br />
+            <div
+            dangerouslySetInnerHTML={{
+                __html: editedFile
+              }}
+            />
         </div>
     );
 
