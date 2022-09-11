82dd95bb94b35ec2449d8c50a0e0352e89c5855c
kugaitomomi <tottomo7@gmail.com>
2022-09-11 19:35:29 +0900

    resolve conflict

---
+++ frontend/src/components/templates/Signup.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { SignUpForm } from "./Interfaces";
+import axios, { AxiosResponse, AxiosError } from "axios";
+
+type Props = {
+  email: string;
+};
+
+const Signup: React.FC<Props> = ({ email }) => {
+  const BASE_URL = "http://localhost:8000";
+
+  const handleSubmit = () => {
+    const form: SignUpForm | null = document.getElementById("signup-form");
+    const email: string = form!["email_signup"]!.value;
+    const password: string = form!["password_signup"]!.value;
+
+    axios({
+      method: "post",
+      url: `${BASE_URL}/user/signup`,
+      data: {
+        email: email,
+        password: password,
+      },
+    })
+      .then((res: AxiosResponse) => {
+        // TODO: Show something when successfully singed up
+        console.log(res.data);
+      })
+      .catch((err: AxiosError<{ error: string }>) => {
+        // TODO: Show something when error caused
+        console.log(err.response!.data);
+      });
+  };
+
+  return (
+    <div className="display">
+      <h1>新規登録</h1>
+      <form id="signup-form">
+        <label htmlFor="email_signup">新規登録</label>
+        <input
+          type="email"
+          value={email}
+          name=""
+          placeholder="youremail@example.com"
+          id="email_signup"
+        />
+        <label htmlFor="password_signup">パスワード</label>
+        <input type="password" name="" id="password_signup" />
+        <input
+          type="button"
+          value="新規登録する"
+          onClick={handleSubmit}
+        ></input>
+      </form>
+    </div>
+  );
+};
+export default Signup;
