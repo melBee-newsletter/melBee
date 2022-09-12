76665069567f78b5c836ccf01ea753bc4d3a8f45
Thomas Hofmann <thomas.t.hofmann@gmail.com>
2022-09-12 09:40:24 +0900

    chore: revert HEAD3

--- frontend/src/components/templates/EditorBox.tsx
+++ frontend/src/components/templates/EditorBox.tsx
@@ -32,7 +32,7 @@ const EditorBox: React.FC = () => {
         initialValue={
           localStorage.melBeeTempStoragedraft
             ? localStorage.melBeeTempStoragedraft
-            : "hi Hiro!!!!!! <img src = 'https://i.ibb.co/Kb5gPZC/melbee.png'>"
+            : "Good job Hiro!! <img src = 'https://i.ibb.co/Kb5gPZC/melbee.png'>"
         }
         init={{
           toolbar_mode: "sliding",
