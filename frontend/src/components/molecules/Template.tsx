7b100c330c3a3b8f74cbf1774aa27b98c63649cb
Thomas Hofmann <thomas.t.hofmann@gmail.com>
2022-09-12 17:47:54 +0900

    feat: click on template to set default

--- frontend/src/components/molecules/Template.tsx
+++ frontend/src/components/molecules/Template.tsx
@@ -1,31 +1,36 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import EditorBox from '../templates/EditorBox';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import EditorBox from "../templates/EditorBox";
 
 type Props = {
-    template: {
-        thumbnail: string;
-        title: string;
-    };
-}
+  template: {
+    thumbnail: string;
+    title: string;
+  };
+};
 
 const Template: React.FC<Props> = ({ template }) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <div className='px-2 pb-2 pt-2' style={{backgroundColor: "pink"}}>
-            <p className='text-base pb-3'>{template.title}</p>
-            <div className='w-max'>
-                <a onClick={(e) => {
-                    e.preventDefault();
-                    navigate('/user/edit');
-                }}>
-                    <img src={template.thumbnail} alt="template" width={200} />
-                </a>
-            </div>
-        </div>
-    );
-
+  return (
+    <div className="px-2 pb-2 pt-2" style={{ backgroundColor: "pink" }}>
+      <p className="text-base pb-3">{template.title}</p>
+      <div className="w-max">
+        <a
+          onClick={(e) => {
+            e.preventDefault();
+            localStorage.setItem(
+              "melBeeTempStoragedraft",
+              "default page! <img src = 'https://drive.tiny.cloud/1/fl35fbae1uoirilftuwgiaq0j9tyhw36quejctjkra1aeap9/0d286852-c67d-4694-9d4d-815aceb001d1'></img><img src = 'https://drive.tiny.cloud/1/fl35fbae1uoirilftuwgiaq0j9tyhw36quejctjkra1aeap9/0d286852-c67d-4694-9d4d-815aceb001d1'></img><img src = 'https://drive.tiny.cloud/1/fl35fbae1uoirilftuwgiaq0j9tyhw36quejctjkra1aeap9/0d286852-c67d-4694-9d4d-815aceb001d1'></img>"
+            );
+            navigate("/user/edit");
+          }}
+        >
+          <img src={template.thumbnail} alt="template" width={200} />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Template;
