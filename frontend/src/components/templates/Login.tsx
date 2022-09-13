c14abeb5d11708cb336b8d6ca307e2c28e1fc4b3
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-09-13 16:27:13 +0900

    fix: delete context-related stuff

--- frontend/src/components/templates/Login.tsx
+++ frontend/src/components/templates/Login.tsx
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import { LogInForm } from "./Interfaces";
 import axios, { AxiosResponse, AxiosError } from "axios";
-import { useUserLogin } from "../../context/userContext";
 
 type Props = {
   email: string;
@@ -9,7 +8,6 @@ type Props = {
 
 const Login: React.FC<Props> = ({ email }) => {
   const BASE_URL = process.env.REACT_APP_PUBLIC_URL || "http://localhost:8000";
-  const { isLoggedIn, setIsLoggedIn, emailCtx, setEmail} = useUserLogin({});
 
   const handleSubmit = () => {
     const form: LogInForm | null = document.getElementById("login-form");
@@ -25,24 +23,12 @@ const Login: React.FC<Props> = ({ email }) => {
       },
     })
       .then((res: AxiosResponse) => {
-        setIsLoggedIn(true);
         console.log(res.data);
-
-      })
-      .then(() => {
-        console.log('----test!!!!!1------')
-        console.log(context.isLoggedIn)
-        console.log(context.setIsLoggedIn)
       })
       .catch((err: AxiosError<{ error: string }>) => {
         console.log(err);
       });
   };
-
-  useEffect(() => {
-    console.log('----test------')
-    console.log(context.isLoggedIn)
-  }, [context.isLoggedIn])
 
   return (
     <div>
