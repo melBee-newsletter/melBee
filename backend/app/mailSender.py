619a349537576ee081512225706102c7a792d535
Julio Quiezi <julioquiezi@gmail.com>
2022-09-13 23:43:58 +0900

    fix: fix credential path

--- backend/app/mailSender.py
+++ backend/app/mailSender.py
@@ -26,6 +26,7 @@ def getService():
     """Shows basic usage of the Gmail API.
     Lists the user's Gmail labels.
     """
+
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -45,7 +46,7 @@ def getService():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'google-credentials.json', SCOPES)
+                "GOOGLE_APPLICATION_CREDENTIALS", SCOPES)
             creds = flow.run_local_server(port=PORT)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
