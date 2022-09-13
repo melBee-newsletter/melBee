0a835c18e43849acc26fdd212336298644316a74
Julio Quiezi <julioquiezi@gmail.com>
2022-09-13 22:16:21 +0900

    fix: fix google credential

--- backend/app/mailSender.py
+++ backend/app/mailSender.py
@@ -18,41 +18,42 @@ import mimetypes
 import base64
 
 # If modifying these scopes, delete the file token.json.
+
 SCOPES = ['https://mail.google.com/']
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "Credentials.json"
-# def getService():
-#     """Shows basic usage of the Gmail API.
-#     Lists the user's Gmail labels.
-#     """
-#     creds = None
-#     # The file token.json stores the user's access and refresh tokens, and is
-#     # created automatically when the authorization flow completes for the first
-#     # time.
 
-#     isDevelopment = False if os.getenv("DATABASE_URL") else True
-#     if isDevelopment:
-#         PORT = 8080
-#     else:
-#         PORT = 0
+def getService():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
 
-#     if os.path.exists('token.json'):
-#         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-#     # If there are no (valid) credentials available, let the user log in.
-#     if not creds or not creds.valid:
-#         if creds and creds.expired and creds.refresh_token:
-#             creds.refresh(Request())
-#         else:
-#             flow = InstalledAppFlow.from_client_secrets_file(
-#                 'google-credentials.json', SCOPES)
-#             creds = flow.run_local_server(port=PORT)
-#         # Save the credentials for the next run
-#         with open('token.json', 'w') as token:
-#             token.write(creds.to_json())
+    isDevelopment = False if os.getenv("DATABASE_URL") else True
+    if isDevelopment:
+        PORT = 8080
+    else:
+        PORT = 0
 
-#     service = build('gmail', 'v1', credentials=creds)
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'google-credentials.json', SCOPES)
+            creds = flow.run_local_server(port=PORT)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
 
-#     return service
+    service = build('gmail', 'v1', credentials=creds)
+
+    return service
 
 
 def create_message(sender, subject, message_body, to=None, bcc=None, cc=None):
