915e05db1b44cb3b8798ecc16c1abd6cc05073d3
Hiro-Kanetake <hiro_k@live.com>
2022-09-14 10:25:01 +0900

    feat: implement hamburger menu render to logged in status only

--- frontend/src/components/organisms/Header.tsx
+++ frontend/src/components/organisms/Header.tsx
@@ -1,11 +1,9 @@
-import React, { useState, useEffect, FC } from "react";
+import React, { useState, FC } from "react";
 import headerLogo from "../atoms/logo.png";
 import ToggleButton from "../molecules/ToggleButton";
 import Navigation from "../molecules/Navigation";
 
 const Header: FC = () => {
-  /*TODO: Implement a proper state management once firebase auth is implemented*/
-  const [loggedIn, setLoggedIn] = useState<boolean>(true);
   const [open, SetOpen] = useState(false);
   const toggleFunction = () => {
     SetOpen((prevState) => {
@@ -13,12 +11,15 @@ const Header: FC = () => {
     });
   };
 
+  const session: null|string = sessionStorage.getItem("isLoggedIn");
+  const isLoggedIn = true ? session != null : false;
+
   return (
     <div className="header drop-shadow-md flex">
       <a href="/" className="px-5">
         <img src={headerLogo} alt="melBee_logo" className="py-2" width="80" />
       </a>
-      {loggedIn ? (
+      {isLoggedIn ? (
         <>
           <ToggleButton
             open={open}
