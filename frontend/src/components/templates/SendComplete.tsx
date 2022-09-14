9011644647d069d1ca236d5a61059da4b79af99e
Hiro-Kanetake <hiro_k@live.com>
2022-09-14 10:16:07 +0900

    style: change text and update buttons

--- frontend/src/components/templates/SendComplete.tsx
+++ frontend/src/components/templates/SendComplete.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import headerLogo from "../atoms/logo.png";
 
 function SendComplete() {
   const session: null|string = sessionStorage.getItem("isLoggedIn");
@@ -8,12 +9,23 @@ function SendComplete() {
   return (
     <>
     {isLoggedIn && 
-    <div id="completeArea" style={{ backgroundColor: "beige" }}>
-      <h3>Send your message!!</h3>
-      <div>
-        <Link to={"/user/send/"}>宛先を追加する &gt;</Link>
-        <Link to={"/user/templates"}>続けて新規作成 &gt;</Link>
+    <div id="completeArea" style={{ backgroundColor: "beige" }} >
+      <div className="flex justify-center">
+        <div>
+          <img src={headerLogo} alt="melbee-logo" className="py-2" width="200" />
+          <h3>送信完了しました</h3>
+          <br />
+        </div>
       </div>
+      <div className="flex justify-around">
+        <div>
+          <Link to={"/user/send/"} className="bg-sky-400 text-white rounded pr-4 pl-4">宛先を追加</Link>
+        </div>
+        <div>
+          <Link to={"/user/templates"} className="bg-amber-300 text-white rounded pr-4 pl-4">新規作成</Link>
+        </div>
+      </div>
+      <br />
     </div>
     }
 
