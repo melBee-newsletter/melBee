54c143218c1f7a58f65c3bb4e4f2cef35db0accd
kugaitomomi <tottomo7@gmail.com>
2022-09-14 13:45:03 +0900

    chore adjustcss

--- frontend/src/Central.tsx
+++ frontend/src/Central.tsx
@@ -1,5 +1,5 @@
-import React, { useState, ReactNode } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom'
+import React, { useState, ReactNode } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import "./App.css";
 import Header from "./components/organisms/Header";
 import Footer from "./components/organisms/Footer";
@@ -8,71 +8,75 @@ type Props = {
   displayComponent: ReactNode;
 };
 
-const Central: React.FC<Props> = ({displayComponent}) => {
+const Central: React.FC<Props> = ({ displayComponent }) => {
   const location = useLocation();
   const navigate = useNavigate();
-  
+
   let currentView = location.pathname;
-  
-  const TEMPLATE_PATH = '/user/templates';
-  const EDIT_PATH = '/user/edit';
-  const PREVIEW_PATH = '/user/preview';
-  const SEND_PATH = '/user/send';
-  const SENT_PATH = '/user/sent';
+
+  const TEMPLATE_PATH = "/user/templates";
+  const EDIT_PATH = "/user/edit";
+  const PREVIEW_PATH = "/user/preview";
+  const SEND_PATH = "/user/send";
+  const SENT_PATH = "/user/sent";
 
   const [editedFile, setEditedFile] = useState<string>("");
-  const session: null|string = sessionStorage.getItem("isLoggedIn");
+  const session: null | string = sessionStorage.getItem("isLoggedIn");
   const isLoggedIn = true ? session != null : false;
 
   return (
     <div className="App">
-      <header className='fixed w-full'>
+      <header className="fixed w-full">
         <Header />
       </header>
 
       <main className="App-header pt-24">
-      {isLoggedIn && 
-        <div className="flex">
-          <div className='contentCenter'>
-            {displayComponent}
-          </div>
-          <div className="contentRight">
-            {(currentView === EDIT_PATH || currentView === PREVIEW_PATH)?
-            <button onClick={(e) => {
-              e.preventDefault();
-              if (currentView === EDIT_PATH) {
-                navigate(PREVIEW_PATH);
-              } else if (currentView === PREVIEW_PATH) {
-                navigate(SEND_PATH);
-              };
-              }} className='text-sm bg-sky-500 text-white pt-2 pb-2 pr-4 pl-4'>
-              {'次に進む >'}
-            </button> : null}
-            <br />
-            
-            {(currentView === EDIT_PATH || currentView === PREVIEW_PATH)?
-            <button onClick={(e) => {
-              e.preventDefault();
-              if (currentView === PREVIEW_PATH) {
-                navigate(EDIT_PATH);
-              } else if (currentView === EDIT_PATH) {
-                navigate(TEMPLATE_PATH);
-              };
-            }} className='text-sm bg-amber-500 text-white pt-2 pb-2 pr-4 pl-4'>
-              {'< 戻る'}
-            </button> : null}
-            <br />
-          </div>
-        </div>
-      }
+        {isLoggedIn && (
+          <div>
+            <div>{displayComponent}</div>
+            <div className="contentRight">
+              {currentView === EDIT_PATH || currentView === PREVIEW_PATH ? (
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    if (currentView === EDIT_PATH) {
+                      navigate(PREVIEW_PATH);
+                    } else if (currentView === PREVIEW_PATH) {
+                      navigate(SEND_PATH);
+                    }
+                  }}
+                  className="text-sm bg-sky-500 text-white pt-2 pb-2 pr-4 pl-4"
+                >
+                  {"次に進む >"}
+                </button>
+              ) : null}
+              <br />
 
-      {!isLoggedIn && 
-        <h1>melBeeはログインした方のみご利用になれます。</h1>
-      }
+              {currentView === EDIT_PATH || currentView === PREVIEW_PATH ? (
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    if (currentView === PREVIEW_PATH) {
+                      navigate(EDIT_PATH);
+                    } else if (currentView === EDIT_PATH) {
+                      navigate(TEMPLATE_PATH);
+                    }
+                  }}
+                  className="text-sm bg-amber-500 text-white pt-2 pb-2 pr-4 pl-4"
+                >
+                  {"< 戻る"}
+                </button>
+              ) : null}
+              <br />
+            </div>
+          </div>
+        )}
+
+        {!isLoggedIn && <h1>melBeeはログインした方のみご利用になれます。</h1>}
       </main>
 
-      <footer className='w-full'>
-      <Footer />
+      <footer className="w-full">
+        <Footer />
       </footer>
     </div>
   );
