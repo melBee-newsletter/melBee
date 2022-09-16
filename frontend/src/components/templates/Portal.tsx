71df717554591d1c7ca78bf31bb3a064595fa231
Hiro-Kanetake <hiro_k@live.com>
2022-09-16 13:42:10 +0900

    feat: import profile, contactlist, senthistory components

--- frontend/src/components/templates/Portal.tsx
+++ frontend/src/components/templates/Portal.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import Profile from "../organisms/Profile";
+import ContactList from "../organisms/ContactList";
+import SentHistory from "../organisms/SentHistory";
 
 const Portal: React.FC = () => {
     return (
-        <div>
+        <div style={{width: 1400, margin: "0 auto"}}>
+            <Profile />
 
+            <ContactList />
+
+            <SentHistory />
         </div>
     );
 };
