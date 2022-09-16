0ef8e483f876d4193afc6520d324d022b13bdccc
Hiro-Kanetake <hiro_k@live.com>
2022-09-16 16:02:26 +0900

    style: fix styling

--- frontend/src/components/organisms/Profile.tsx
+++ frontend/src/components/organisms/Profile.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 type Event = {
     target: {
@@ -16,76 +17,82 @@ type Props = {
 };
 
 const Profile: React.FC<Props> = ({ analytics, setAnalytics }) => {
-    const DOWN = "rotate-90";
-    const UP = "-rotate-90";
-    const [expand, setExpand] = useState<boolean>(false);
-    const [direction, setDirection] = useState<string>(DOWN);
+  const DOWN = "rotate-90";
+  const UP = "-rotate-90";
+  const [expand, setExpand] = useState<boolean>(false);
+  const [direction, setDirection] = useState<string>(DOWN);
+  const navigate = useNavigate();
 
-    const handleExpand = (e: any) => {
-        e.preventDefault();
-        setExpand(!expand);
-    };
+  const handleExpand = (e: any) => {
+      e.preventDefault();
+      setExpand(!expand);
+  };
 
-    useEffect(() => {
-        !expand ? setDirection(DOWN) : setDirection(UP);
-    }, [expand]);
+  useEffect(() => {
+      !expand ? setDirection(DOWN) : setDirection(UP);
+  }, [expand]);
 
-    const [analyticsEdit, setAnalyticsEdit] = useState(true);
+  const [analyticsEdit, setAnalyticsEdit] = useState(true);
 
-    const handleChange = (event: Event) => {
-        setAnalytics(event.target.value);
-    };
+  const handleChange = (event: Event) => {
+      setAnalytics(event.target.value);
+  };
 
-    const handleClick = (event: clickEvent) => {
-        event.preventDefault();
-        setAnalytics(analytics);
-        setAnalyticsEdit(false);
-    };
+  const handleClick = (event: clickEvent) => {
+      event.preventDefault();
+      setAnalytics(analytics);
+      setAnalyticsEdit(false);
+  };
 
-    const handleEdit = (event: clickEvent) => {
-        event.preventDefault();
-        setAnalyticsEdit(true);
-    };
+  const handleEdit = (event: clickEvent) => {
+      event.preventDefault();
+      setAnalyticsEdit(true);
+  };
 
-    return (
-        <div className="bg-sky-200 justify-center">
-            <div className="flex justify-between px-10 py-3 text-xl">
-                <h3>登録情報</h3>
-                <button className={direction} onClick={handleExpand}>▷</button>
-            </div>
-            {(expand) && (
-                <div className="flex justify-center">
-                <div className="bg-white w-full">
-                    <p>登録内容</p>
-                </div>
+  return (
+    <>
+      <div>
+        <button onClick={(e) => { navigate("/user/templates")}} className="bg-yellow-300 rounded-xl px-4 my-10">新規作成</button>
+      </div>
+      <div className="bg-sky-200 justify-center">
+          <div className="flex justify-between px-10 py-3 text-xl">
+              <h3>登録情報</h3>
+              <button className={direction} onClick={handleExpand}>▷</button>
+          </div>
+          {(expand) && (
+              <div className="flex justify-center">
+              <div className="bg-white w-full">
+                  <p>登録内容</p>
                 <div>
-                {analyticsEdit ? (
-        <div>
-          <label>Analyticsタグの入力:</label>
-          <input
-            type="text"
-            onChange={handleChange}
-            value={analytics}
-            placeholder="Google Analyticsタグ"
-          ></input>
-          <button onClick={handleClick}>確定</button>
-        </div>
-      ) : (
-        <div>
-          <h3>Google Analyticsは設定済みです</h3>
-          <button
-            className="bg-yellow-300 pr-4 pl-4 rounded"
-            onClick={handleEdit}
-          >
-            Analyticsタグを編集する
-          </button>
-        </div>
-      )}
+                  {analyticsEdit ? (
+                    <div>
+                      <label>Analyticsタグの入力:</label>
+                      <input
+                        type="text"
+                        onChange={handleChange}
+                        value={analytics}
+                        placeholder="Google Analyticsタグ"
+                      ></input>
+                      <button onClick={handleClick} className="bg-sky-300 pr-4 pl-4 rounded-xl" >確定</button>
+                    </div>
+                  ) : (
+                  <div className="justify-center">
+                    <h3>Google Analyticsは設定済みです</h3>
+                    <button
+                      className="bg-yellow-300 pr-4 pl-4 rounded-xl"
+                      onClick={handleEdit}
+                    >
+                      編集
+                    </button>
+                  </div>
+                  )}
                 </div>
-            </div>
-            )}
-        </div>
-    );
+              </div>
+          </div>
+          )}
+      </div>
+    </>
+  );
 };
 
 export default Profile;
