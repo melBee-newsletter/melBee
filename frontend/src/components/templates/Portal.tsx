13b3f683fa223253c8293e38564e9effccea5c4d
Hiro-Kanetake <hiro_k@live.com>
2022-09-16 21:39:52 +0900

    feat: add mytemplate component

--- frontend/src/components/templates/Portal.tsx
+++ frontend/src/components/templates/Portal.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Profile from "../organisms/Profile";
 import ContactList from "../organisms/ContactList";
+import MyTemplates from "../organisms/MyTemplates";
 import SentHistory from "../organisms/SentHistory";
 
 type Props = {
@@ -9,12 +11,22 @@ type Props = {
 };
 
 const Portal: React.FC<Props> = ({ analytics, setAnalytics }) => {
+    const navigate = useNavigate();
+
     return (
-        <div style={{width: 1400, margin: "0 auto"}}>
+        <div style={{width: "100vw", margin: "0 auto"}} className="px-10">
+            <div>
+                <button onClick={(e) => { navigate("/user/templates")}} className="bg-yellow-400 rounded-xl px-4 my-10 border-solid border-black border-4">
+                    新規作成
+                </button>
+            </div>
+
             <Profile analytics={analytics} setAnalytics={setAnalytics} />
 
             <ContactList />
 
+            <MyTemplates />
+            
             <SentHistory />
         </div>
     );
