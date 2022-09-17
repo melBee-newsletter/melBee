333d83b784d0ced7b7e3adc0ca9fb03574d1e177
Hiro-Kanetake <hiro_k@live.com>
2022-09-17 13:21:30 +0900

    feat: set fetch mytemplates from backend

--- frontend/src/components/organisms/MyTemplates.tsx
+++ frontend/src/components/organisms/MyTemplates.tsx
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from "react";
+import axios, { AxiosResponse, AxiosError }  from "axios";
+import Template from "../molecules/Template";
+
+type template = {
+    id: number;
+    thumbnail: string;
+    title: string;
+};
 
 const MyTemplates: React.FC = () => {
+    const BASE_URL = process.env.REACT_APP_PUBLIC_URL || "http://localhost:8000";
     const DOWN = "rotate-90";
     const UP = "-rotate-90";
     const [expand, setExpand] = useState<boolean>(false);
     const [direction, setDirection] = useState<string>(DOWN);
+    const [templates, setTemplates] = useState<template[]>([]);
 
     const handleExpand = (e: any) => {
         e.preventDefault();
@@ -15,6 +25,23 @@ const MyTemplates: React.FC = () => {
         !expand ? setDirection(DOWN) : setDirection(UP);
     }, [expand]);
 
+    const getSavedTemplate = () => {
+        axios({
+          method: "get",
+          url: `${BASE_URL}/user/${sessionStorage.melbeeID}/template`,
+        })
+        .then((res: AxiosResponse) => {
+            console.log(res.data);
+            let data = res.data;
+        data.map((singleTemplate: template) => {
+            setTemplates((current) => [singleTemplate, ...current]);
+        });
+        })
+        .catch((err: AxiosError<{ error: string }>) => {
+            console.log(err.response!.data);
+        });
+    };
+
     return (
         <div className="bg-neutral-500 my-2">
             <div className="flex justify-between px-10 py-3 text-xl text-white">
@@ -24,7 +51,11 @@ const MyTemplates: React.FC = () => {
             {(expand) && (
             <div className="flex justify-center">
                 <div className="bg-white w-full">
-                    <p>テンプレート</p>
+                    <div className="grid grid-cols-4 gap-4 bg-black mx-5 my-4 rounded-lg">
+                        {templates.map((template) => {
+                            return <Template template={template} />;
+                        })}
+                    </div>
                 </div>
             </div>
             )}
