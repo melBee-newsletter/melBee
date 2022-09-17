215b392c7a6ee13779b4673ecd63cbaeb6232ab0
Hiro-Kanetake <hiro_k@live.com>
2022-09-17 15:21:45 +0900

    refactor: implement fetching and click to select function

--- frontend/src/components/templates/TemplateBox.tsx
+++ frontend/src/components/templates/TemplateBox.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Template from "../molecules/Template";
-import templateImage from "../atoms/template_sample.png";
 import axios, { AxiosResponse, AxiosError } from "axios";
+import { useNavigate } from "react-router-dom";
 
 type template = {
   id: number;
@@ -10,16 +10,23 @@ type template = {
 };
 
 const TemplateBox: React.FC = () => {
-  const tempArr: template[] = [];
-  const [templates, setTemplates] = useState<template[]>([]);
+  const [melBeeTemplates, setMelBeeTemplates] = useState<template []>([]);
+  const [myTemplates, setMyTemplates] = useState<template []>([]);
+  const [fetchTemplate, setFetchTemplate] = useState<boolean>(false);
+  // const [loading, setLoading] = useState<boolean>(true);
   const BASE_URL = process.env.REACT_APP_PUBLIC_URL || "http://localhost:8000";
+  const navigate = useNavigate();
 
+  const numOfTemplates = 4;
   const seedTemplate = () => {
     axios({
       method: "post",
       url: `${BASE_URL}/template/seed`,
       data: "tomatoTest",
     });
+    for (let i = 1; i <= numOfTemplates; i++) {
+      getTemplate(i);
+    };
   };
 
   const getTemplate = (id: number) => {
@@ -30,8 +37,7 @@ const TemplateBox: React.FC = () => {
       .then((res: AxiosResponse) => {
         let data = res.data;
         data.id = id;
-        console.log(data);
-        setTemplates((current) => [...current, data]);
+        setMelBeeTemplates((current) => [...current, data]);
       })
       .catch((err: AxiosError<{ error: string }>) => {
         console.log(err.response!.data);
@@ -44,10 +50,9 @@ const TemplateBox: React.FC = () => {
       url: `${BASE_URL}/user/${sessionStorage.melbeeID}/template`,
     })
       .then((res: AxiosResponse) => {
-        console.log(res.data);
         let data = res.data;
-        data.map((singleTemplate: template) => {
-          setTemplates((current) => [singleTemplate, ...current]);
+        data.map((template: template) => {
+          setMyTemplates((current) => [template, ...current]);
         });
       })
       .catch((err: AxiosError<{ error: string }>) => {
@@ -57,25 +62,66 @@ const TemplateBox: React.FC = () => {
 
   useEffect(() => {
     seedTemplate();
-  }, []);
-
-  useEffect(() => {
-    for (let i = 1; i <= 5; i++) {
-      getTemplate(i);
-    }
-  }, []);
-
-  useEffect(() => {
     getSavedTemplate();
+    setFetchTemplate(true);
   }, []);
+
+  // TODO: if possible, render a loading component until the fetching is done.
+  // useEffect(() => {
+  //   setLoading(false);
+  // }, [fetchTemplate]);
+
+  const handleMyTemplate = (i: number) => {
+    axios({
+      method: "get",
+      url: `${BASE_URL}/user/${sessionStorage.melbeeID}/template`,
+    })
+    .then((res: AxiosResponse) => {
+      let data = res.data;
+      localStorage.setItem("melBeeTempStoragedraft", data[i].body);
+    })
+    .then(() => navigate("/user/edit"));
+  };
+
+  const handleMelBeeTemplate = (i: number) => {
+    const templateId = melBeeTemplates[i].id;
+    axios({
+      method: "get",
+      url: `${BASE_URL}/template/${templateId}`,
+    })
+    .then((res) => {
+      const data = res.data;
+      localStorage.setItem("melBeeTempStoragedraft", data.body);
+    })
+    .then(() => navigate("/user/edit"));
+  };
 
   return (
-    <div style={{ backgroundColor: "gray" }}>
+    <div className="bg-white w-screen mx-80">
       <h3>テンプレートをお選びください</h3>
-      <div className="px-5 py-3">
-        <div className="grid gap-4 grid-cols-4 grid-rows-4">
-          {templates.map((template) => {
-            return <Template template={template} />;
+      <div className="px-5 py-3 mx-20">
+        カスタマイズされたテンプレート
+        <div className="grid gap-4 grid-cols-4">
+          {myTemplates.map((template, i) => {
+            return (
+              <a key={`myTemp${i}`} onClick={(e)=> {
+                e.preventDefault();
+                handleMyTemplate(i)}}>
+                <Template template={template} />
+              </a>
+            )
+          })}
+        </div>
+        melBee テンプレート
+        <div className="grid gap-4 grid-cols-4">
+          {melBeeTemplates.map((template, i) => {
+            return (
+              <a key={`mbTemp${i}`} onClick={(e)=> {
+                e.preventDefault();
+                handleMelBeeTemplate(i)}}>
+                <Template template={template} />
+              </a>
+            ) 
           })}
         </div>
       </div>
