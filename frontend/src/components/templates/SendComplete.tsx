ab6fa42d7fa908385a1dc906a7f3b20c5c3a9b27
Hiro-Kanetake <hiro_k@live.com>
2022-09-19 17:35:07 +0900

    feat: implement conditions based on send limit

--- frontend/src/components/templates/SendComplete.tsx
+++ frontend/src/components/templates/SendComplete.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import headerLogo from "../atoms/logo.png";
 
-function SendComplete() {
+type Props = {
+  reachLimit: boolean;
+};
+
+const SendComplete: React.FC<Props> = ({ reachLimit }) => {
   const session: null|string = sessionStorage.getItem("isLoggedIn");
   const isLoggedIn = true ? session != null : false;
 
@@ -15,14 +19,22 @@ function SendComplete() {
           <br />
         </div>
       </div>
-      <div className="flex justify-around">
+      {!reachLimit ? <div className="flex justify-around">
         <div>
           <Link to={"/user/send/"} className="bg-sky-400 text-white rounded px-4 mr-5">宛先を追加</Link>
         </div>
         <div>
           <Link to={"/user/templates"} className="bg-amber-300 text-white rounded px-4 ml-5">新規作成</Link>
         </div>
-      </div>
+      </div> : 
+      (<div className="block justify-center">
+        <div>
+          <h3>本日の送信リミットに達しましたが、引き続きテンプレート作成はご利用いただけます。</h3>
+        </div>
+        <div>
+          <Link to={"/user/templates"} className="bg-amber-300 text-white rounded px-4 ml-5">新規作成</Link>
+        </div>
+      </div>)}
       <br />
     </>
   );
