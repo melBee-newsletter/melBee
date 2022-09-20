de908bc34a7cdf799413dbcf497a7060d79d4c10
kugaitomomi <tottomo7@gmail.com>
2022-09-20 17:41:49 +0900

    chore: CSS adjustment in progress

--- frontend/src/components/organisms/Header.tsx
+++ frontend/src/components/organisms/Header.tsx
@@ -11,27 +11,34 @@ const Header: FC = () => {
     });
   };
 
-  const session: null|string = sessionStorage.getItem("isLoggedIn");
+  const session: null | string = sessionStorage.getItem("isLoggedIn");
   const isLoggedIn = true ? session != null : false;
   const logoLink = isLoggedIn ? "/user" : "/";
 
   return (
-    <div className="header drop-shadow-md flex">
-      <a href={logoLink} className="px-5">
-        <img src={headerLogo} alt="melBee_logo" className="py-2" width="80" />
-      </a>
-
-      {isLoggedIn && (
-        <>
-          <ToggleButton
-            open={open}
-            controls="navigation"
-            label="メニューを開きます"
-            onClick={toggleFunction}
+    <div className="header drop-shadow-md">
+      <div className="flex">
+        <a href={logoLink} className="px-5">
+          <img
+            src={headerLogo}
+            alt="melBee_logo"
+            className="py-2 logo"
+            width="80"
           />
-          <Navigation id="navigation" open={open} />
-        </>
-      )}
+        </a>
+
+        {isLoggedIn && (
+          <>
+            <ToggleButton
+              open={open}
+              controls="navigation"
+              label="メニューを開きます"
+              onClick={toggleFunction}
+            />
+            <Navigation id="navigation" open={open} />
+          </>
+        )}
+      </div>
     </div>
   );
 };
