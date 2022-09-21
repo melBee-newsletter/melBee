334b81e3948bddb7107ba1acdd7210043602f5e8
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-09-21 15:31:00 +0900

    fix: make function names clear

--- backend/app/main.py
+++ backend/app/main.py
@@ -56,7 +56,7 @@ def get_user(id: int, db: Session = Depends(get_db)):
 
 
 @app.get("/user/{id}/template")
-def get_user(id: int, db: Session = Depends(get_db)):
+def get_template_by_user_id(id: int, db: Session = Depends(get_db)):
     templateuser = crud.get_user_template(db, id)
     if not templateuser:
         raise HTTPException(status_code=400, detail="Invalid id. 無効なidです。")
@@ -64,7 +64,7 @@ def get_user(id: int, db: Session = Depends(get_db)):
 
 
 @app.post("/user/{id}/template", response_model={})
-def add_user_template(id: int, template: schemas.TemplateBase, db: Session = Depends(get_db)):
+def add_template_by_user_id(id: int, template: schemas.TemplateBase, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, id)
     if not db_user:
         raise HTTPException(
@@ -74,7 +74,7 @@ def add_user_template(id: int, template: schemas.TemplateBase, db: Session = Dep
 
 
 @app.get("/user/{id}/sent_history")
-def get_sent_history(id: int, db: Session = Depends(get_db)):
+def get_sent_history_by_user_id(id: int, db: Session = Depends(get_db)):
     userhistory = crud.get_user_history(db, id)
     if not userhistory:
         raise HTTPException(
@@ -150,7 +150,7 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 
 @app.post("/user/login", response_model=schemas.User)
-def create_user(user: schemas.UserVerify, db: Session = Depends(get_db)):
+def log_in_with_id_and_pw(user: schemas.UserVerify, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
     if not db_user:
         raise HTTPException(
