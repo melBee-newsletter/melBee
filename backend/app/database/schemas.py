18186caf6fedff107b85e14935e6fb07c8e5cd10
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-09-22 10:32:37 +0900

    fix: delete non used schemas

--- backend/app/database/schemas.py
+++ backend/app/database/schemas.py
@@ -4,22 +4,6 @@ from pydantic import BaseModel
 from typing import List
 
 
-class ItemBase(BaseModel):
-    title: str
-    description: Optional[str] = None
-
-
-class ItemCreate(ItemBase):
-    pass
-
-
-class Item(ItemBase):
-    id: int
-    owner_id: int
-
-    class Config:
-        orm_mode = True
-
 # ---- User ----- #
 
 
