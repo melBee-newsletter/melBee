41e0c0cb8e0cd954657edb1e4dd8cea9384f43c6
kugaitomomi <tottomo7@gmail.com>
2022-09-23 13:57:54 +0900

    chore adjust footer

--- frontend/src/pages/User/SendBox.tsx
+++ frontend/src/pages/User/SendBox.tsx
@@ -3,6 +3,7 @@ import axios, { AxiosResponse, AxiosError } from "axios";
 import Loading from "../../components/Loading";
 import SendComplete from "./components/SendComplete";
 import { useNavigate } from "react-router-dom";
+import "../../sendbox.css";
 
 type Props = {
   analytics: string;
@@ -14,7 +15,7 @@ interface contact {
   email: string;
   id: number;
   is_subscribed: boolean;
-};
+}
 
 type history = {
   date_sent: string;
@@ -30,33 +31,38 @@ const SendBox: React.FC<Props> = ({ analytics, reachLimit, setCountSent }) => {
   const [allEmails, setAllEmails] = useState<string[]>([]);
   const [email, setEmail] = useState<string>("");
   const [receivers, setReceivers] = useState<string[]>([]);
-  const [isChecked, setIsChecked] = useState<boolean[]>(new Array(allEmails.length).fill(false));
+  const [isChecked, setIsChecked] = useState<boolean[]>(
+    new Array(allEmails.length).fill(false)
+  );
   const [selectAll, setSelectAll] = useState<boolean>(false);
   const [updateReceiver, setUpdateReceiver] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
   const [sendComplete, setSendComplete] = useState<boolean>(false);
 
-  let TEMPLATE = localStorage.melBeeTempStoragedraft
+  let TEMPLATE = localStorage.melBeeTempStoragedraft;
   const DATE = new Date();
-  
+
   useEffect(() => {
-    if (analytics) TEMPLATE += `<img src=https://www.google-analytics.com/collect?v=1&tid=${analytics}&cid=555&t=event&ec=emails&ea=open&dt=testemail>`;
+    if (analytics)
+      TEMPLATE += `<img src=https://www.google-analytics.com/collect?v=1&tid=${analytics}&cid=555&t=event&ec=emails&ea=open&dt=testemail>`;
     axios({
       method: "get",
       url: `${BASE_URL}/user/${sessionStorage.melbeeID}/contact_list`,
     })
-    .then((res: AxiosResponse) => {
-      let data = res.data;
-      const notBlackList = data.filter((contact: contact) => contact.is_subscribed);
-      notBlackList.map((contact: contact) => {
-        const email = contact.email;
-        setAllEmails((prevEmail) => [...prevEmail, email]);
-        setIsChecked((prevStat) => [...prevStat, false]);
+      .then((res: AxiosResponse) => {
+        let data = res.data;
+        const notBlackList = data.filter(
+          (contact: contact) => contact.is_subscribed
+        );
+        notBlackList.map((contact: contact) => {
+          const email = contact.email;
+          setAllEmails((prevEmail) => [...prevEmail, email]);
+          setIsChecked((prevStat) => [...prevStat, false]);
+        });
+      })
+      .catch((err: AxiosError<{ error: string }>) => {
+        console.log(err.response!.data);
       });
-    })
-    .catch((err: AxiosError<{ error: string }>) => {
-      console.log(err.response!.data);
-    });
   }, []);
 
   if (!localStorage.getItem("subject")) {
@@ -67,9 +73,11 @@ const SendBox: React.FC<Props> = ({ analytics, reachLimit, setCountSent }) => {
     setSubject(subject);
     localStorage.setItem("subject", subject);
   };
-  
+
   const handleCheck = (position: any | void) => {
-    const updateIsChecked = isChecked.map((stat, i) => i === position ? !stat : stat);
+    const updateIsChecked = isChecked.map((stat, i) =>
+      i === position ? !stat : stat
+    );
     setIsChecked(updateIsChecked);
     setUpdateReceiver(!updateReceiver);
   };
@@ -88,29 +96,40 @@ const SendBox: React.FC<Props> = ({ analytics, reachLimit, setCountSent }) => {
         email: email,
         user_id: sessionStorage.melbeeID,
         is_subscribed: true,
-      }
+      };
       axios({
         method: "post",
         url: `${BASE_URL}/user/contact_list`,
         data: data,
       })
-      .then((res: AxiosResponse) => {
-        setAllEmails((prevEmail) => [...prevEmail, email]);
-        setIsChecked((prevStat) => [...prevStat, true]);
-        setUpdateReceiver(!updateReceiver);
-      })
-      .catch((err: AxiosError<{ error: string }>) => {
-        alert("ご入力されたメールアドレスはすでに連絡先に登録されているか、配信停止となっております。");
-        console.log(err.response!.data);
-      });      
+        .then((res: AxiosResponse) => {
+          setAllEmails((prevEmail) => [...prevEmail, email]);
+          setIsChecked((prevStat) => [...prevStat, true]);
+          setUpdateReceiver(!updateReceiver);
+        })
+        .catch((err: AxiosError<{ error: string }>) => {
+          alert(
+            "ご入力されたメールアドレスはすでに連絡先に登録されているか、配信停止となっております。"
+          );
+          console.log(err.response!.data);
+        });
       setEmail("");
-    };
+    }
   };
 
   const displayEmailWithCheckbox = (email: string, i: number) => {
     return (
-      <div key={i} className="flex rounded-lg mr-3 px-3 py-1 h-fit text-base my-1">
-        <input type="checkbox" id={String(i)} checked={isChecked[i]} onChange={()=>handleCheck(i)} className="mr-2" />
+      <div
+        key={i}
+        className="flex rounded-lg mr-3 px-3 py-1 h-fit text-base my-1"
+      >
+        <input
+          type="checkbox"
+          id={String(i)}
+          checked={isChecked[i]}
+          onChange={() => handleCheck(i)}
+          className="mr-2"
+        />
         <p>{email}</p>
       </div>
     );
@@ -118,7 +137,7 @@ const SendBox: React.FC<Props> = ({ analytics, reachLimit, setCountSent }) => {
 
   useEffect(() => {
     const selectedEmails = allEmails.filter((email, i) => {
-      return (isChecked[i]);
+      return isChecked[i];
     });
     setReceivers(selectedEmails);
   }, [updateReceiver]);
@@ -138,63 +157,63 @@ const SendBox: React.FC<Props> = ({ analytics, reachLimit, setCountSent }) => {
         url: `${BASE_URL}/email/send`,
         data: data,
       })
-      .then((res: AxiosResponse) => {
-        setSendComplete(true);
-        setLoading(false);
-        axios({
-          method: "post",
-          url: `${BASE_URL}/user/${sessionStorage.melbeeID}/sent_history`,
-          data: {
-            recipients: JSON.stringify(receivers),
-            template: TEMPLATE,
-            date_sent: DATE.toString(),
-            user_id: sessionStorage.melbeeID,
-            subject: subject
-          }
-        })
-        .catch((err: AxiosError<{ error: string }>) => {
-          console.log(err.response!.data);
-        });
-      }).then(() => {
-        const TODAY = {
-          year: DATE.getFullYear(),
-          month: DATE.getMonth() + 1,
-          day: DATE.getDay(),
-        };
-    
-        let newCountSent = 0;
-    
-        const checkSentDate = (stringDate: string) => {
-          const sentDate = new Date(stringDate);
-          if (
-            sentDate.getFullYear() === TODAY.year &&
-            sentDate.getMonth() + 1 === TODAY.month &&
-            sentDate.getDay() === TODAY.day
-          ) {
-            setCountSent(newCountSent += 1);
-          }
-        };
-    
-        axios({
-          method: "get",
-          url: `${BASE_URL}/user/${sessionStorage.melbeeID}/sent_history`,
-        })
-          .then((res: AxiosResponse) => {
-            let data = res.data;
-            data.map((history: history) => {
-              checkSentDate(history.date_sent);
-            });
-          })
-          .catch((err: AxiosError<{ error: string }>) => {
+        .then((res: AxiosResponse) => {
+          setSendComplete(true);
+          setLoading(false);
+          axios({
+            method: "post",
+            url: `${BASE_URL}/user/${sessionStorage.melbeeID}/sent_history`,
+            data: {
+              recipients: JSON.stringify(receivers),
+              template: TEMPLATE,
+              date_sent: DATE.toString(),
+              user_id: sessionStorage.melbeeID,
+              subject: subject,
+            },
+          }).catch((err: AxiosError<{ error: string }>) => {
             console.log(err.response!.data);
           });
-      })
-      .catch((err: AxiosError<{ error: string }>) => {
-        alert(
-          "エラーが生じました。お宛先のメールアドレス及び件名を今一度ご確認ください。"
-        );
-        console.log(err.response!.data);
-      });
+        })
+        .then(() => {
+          const TODAY = {
+            year: DATE.getFullYear(),
+            month: DATE.getMonth() + 1,
+            day: DATE.getDay(),
+          };
+
+          let newCountSent = 0;
+
+          const checkSentDate = (stringDate: string) => {
+            const sentDate = new Date(stringDate);
+            if (
+              sentDate.getFullYear() === TODAY.year &&
+              sentDate.getMonth() + 1 === TODAY.month &&
+              sentDate.getDay() === TODAY.day
+            ) {
+              setCountSent((newCountSent += 1));
+            }
+          };
+
+          axios({
+            method: "get",
+            url: `${BASE_URL}/user/${sessionStorage.melbeeID}/sent_history`,
+          })
+            .then((res: AxiosResponse) => {
+              let data = res.data;
+              data.map((history: history) => {
+                checkSentDate(history.date_sent);
+              });
+            })
+            .catch((err: AxiosError<{ error: string }>) => {
+              console.log(err.response!.data);
+            });
+        })
+        .catch((err: AxiosError<{ error: string }>) => {
+          alert(
+            "エラーが生じました。お宛先のメールアドレス及び件名を今一度ご確認ください。"
+          );
+          console.log(err.response!.data);
+        });
     } else {
       alert("送信先を選択してください");
     }
@@ -202,70 +221,90 @@ const SendBox: React.FC<Props> = ({ analytics, reachLimit, setCountSent }) => {
 
   return (
     <>
-    {loading && <Loading word={"S E N D I N G"} />}
-    {sendComplete && <SendComplete reachLimit={reachLimit} setSendComplete={setSendComplete} />}
-    {reachLimit && !sendComplete && 
-      (<div className="flex justify-center">
-        <h3>申し訳ございません、本日の送信リミットに達しました。</h3>
-      </div>)}
-    {!reachLimit && !sendComplete &&
-      <div className="sendArea">
-        <div className="flex items-center justify-start pl-6">
-          <h3 className="text-xl mr-3">件名 :</h3>
-          <input
-            type="text"
-            placeholder="件名"
-            onChange={(e) => handleSubject(e.target.value)}
-            id="subjectId"
-            value={subject}
-            className="border rounded-lg p-2 text-lg"
-            style={{width: 800}}
-          />
+      {loading && <Loading word={"S E N D I N G"} />}
+      {sendComplete && (
+        <SendComplete
+          reachLimit={reachLimit}
+          setSendComplete={setSendComplete}
+        />
+      )}
+      {reachLimit && !sendComplete && (
+        <div className="flex justify-center">
+          <h3>申し訳ございません、本日の送信リミットに達しました。</h3>
         </div>
-        <h3 className="text-xl mt-6 mb-2">送信先メールアドレスをお選びください</h3>
-        <div className="w-full scroll flex flex-wrap content-start border items-start rounded-xl my-3 px-3 py-1" 
-          style={{height: 300}}>
-          {allEmails.map((email, i) => {
-            return displayEmailWithCheckbox(email, i);
-          })}
-        </div>
-        <div className="flex justify-between items-center">
-          <div className="flex ml-10 text-xl">
-            <input type="checkbox" onChange={handleCheckAll} disabled={!allEmails.length} />
-              {!selectAll ? <p className="text-base ml-2">すべて選択</p> : <p className="text-base ml-2">すべて解除</p> }
+      )}
+      {!reachLimit && !sendComplete && (
+        <div className="sendArea pt-24 w-11/12 mx-auto ">
+          <div className="flex items-center justify-start pl-6">
+            <h3 className="text-xl mr-3">件名 :</h3>
+            <input
+              type="text"
+              placeholder="件名"
+              onChange={(e) => handleSubject(e.target.value)}
+              id="subjectId"
+              value={subject}
+              className="border rounded-lg p-2 text-lg sendTitle"
+            />
           </div>
-      
-          <div className="pr-10">
-            <form onSubmit={handleAdd}>
+          <h3 className="text-xl mt-6 mb-2">
+            送信先メールアドレスをお選びください
+          </h3>
+          <div className="scroll flex flex-wrap border items-start rounded-xl my-3 px-3 py-1">
+            {allEmails.map((email, i) => {
+              return displayEmailWithCheckbox(email, i);
+            })}
+          </div>
+          <div className="flex justify-between items-center">
+            <div className="flex text-xl">
               <input
-              className="border rounded-lg p-2 text-base"
-              type="email"
-              value={email}
-              placeholder="メールアドレス"
-              onChange={(e) => setEmail(e.target.value)}
-              style={{width: 350}}
+                type="checkbox"
+                onChange={handleCheckAll}
+                disabled={!allEmails.length}
               />
-              <button className="rounded-xl px-6 py-2 drop-shadow-xl text-lg text-white font-medium bg-orangeGradation ml-3 w-28"> 追加 </button>
-            </form>
-          </div>    
+              {!selectAll ? (
+                <p className="text-base ml-2">すべて選択</p>
+              ) : (
+                <p className="text-base ml-2">すべて解除</p>
+              )}
+            </div>
+
+            <div className="">
+              <form onSubmit={handleAdd}>
+                <input
+                  className="border rounded-lg p-2 text-base"
+                  type="email"
+                  value={email}
+                  placeholder="メールアドレス"
+                  onChange={(e) => setEmail(e.target.value)}
+                  style={{ width: 350 }}
+                />
+                <button className="rounded-xl px-6 py-2 drop-shadow-xl text-lg text-white font-medium bg-orangeGradation ml-3 w-28">
+                  {" "}
+                  追加{" "}
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className="flex justify-end mt-6 text-lg">
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                navigate("/user/preview");
+              }}
+              className="rounded-xl px-6 py-2 drop-shadow-xl text-lg text-white font-medium bg-grayGradation ml-3 w-28"
+            >
+              戻る
+            </button>
+            <button
+              type="submit"
+              onClick={handleSend}
+              className="rounded-xl px-6 py-2 drop-shadow-xl text-lg text-white font-medium bg-blueGradation ml-3 w-36"
+            >
+              送信
+            </button>
+          </div>
         </div>
-        <div className="flex justify-end mt-6 text-lg pr-10">
-          <button 
-            onClick={(e) => {
-              e.preventDefault();
-              navigate("/user/preview")
-            }}
-            className="rounded-xl px-6 py-2 drop-shadow-xl text-lg text-white font-medium bg-grayGradation ml-3 w-28">
-            戻る
-          </button>
-          <button
-            type="submit"
-            onClick={handleSend}
-            className="rounded-xl px-6 py-2 drop-shadow-xl text-lg text-white font-medium bg-blueGradation ml-3 w-36">
-            送信
-          </button>
-        </div>
-      </div>}
+      )}
     </>
   );
 };
