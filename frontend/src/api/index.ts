40eb4637e0366d7171a841dc2153e3d26b62d5b4
Hiro-Kanetake <hiro_k@live.com>
2022-09-23 14:18:08 +0900

    feat: set checkEmail in root api

---
+++ frontend/src/api/index.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosResponse, AxiosError } from "axios";
+const BASE_URL = process.env.REACT_APP_PUBLIC_URL || "http://localhost:8000";
+
+export const checkEmail = async (email: string) => {
+    let emailFound = false;
+    await axios({
+        method: "post",
+        url: `${BASE_URL}/user/check`,
+        data: {
+          email: email,
+        },
+      })
+    .then((res: AxiosResponse) => {
+        if (res.data["isUserSignnedUp"] === true) emailFound = true;
+    })
+    .catch((err: AxiosError<{ error: string }>) => {
+        console.log(err.response!.data);
+    });
+
+    return emailFound;
+};
