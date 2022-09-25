5c3693b7239289660b65387bbc79b43d216e19d9
kugaitomomi <tottomo7@gmail.com>
2022-09-26 08:57:51 +0900

    chore remove class name

--- frontend/src/pages/User/Portal.tsx
+++ frontend/src/pages/User/Portal.tsx
@@ -46,50 +46,53 @@ const Portal: React.FC<Props> = ({
 
   return (
     <div>
-      {loading ? <Loading word={"L O A D I N G"} /> :
-      <div className="portalContent pt-20 mb-40">
-        <div className="flex justify-between mb-6">
-          <div className="text-left">
-            <h2 className="font-bold lg:text-3xl sm:text-[1.6rem]">
-              melBeeへようこそ!
-            </h2>
-            {showLimit && (
-              <p className="lg:mt-1 lg:text-lg font-bold">{todayMessage}</p>
-            )}
-            <div className="mt-2 sm:text-sm">
-              {!showLimit ? (
-                <p className="my-4"> </p>
-              ) : !reachLimit ? (
-                <span>
-                  本日 <strong>{countSent} 通</strong> 送信されました。残り{" "}
-                  <strong>{sendLimit - countSent} 通</strong> 送信できます。
-                </span>
-              ) : (
-                <span>
-                  本日の送信リミットに達しましたが、引き続きテンプレート作成はご利用いただけます。
-                </span>
+      {loading ? (
+        <Loading word={"L O A D I N G"} />
+      ) : (
+        <div className="portalContent pt-20 mb-40">
+          <div className="flex justify-between mb-6">
+            <div className="text-left">
+              <h2 className="font-bold lg:text-3xl sm:text-[1.6rem]">
+                melBeeへようこそ!
+              </h2>
+              {showLimit && (
+                <p className="lg:mt-1 lg:text-lg font-bold">{todayMessage}</p>
               )}
+              <div className="mt-2">
+                {!showLimit ? (
+                  <p className="my-4"> </p>
+                ) : !reachLimit ? (
+                  <span>
+                    本日 <strong>{countSent} 通</strong> 送信されました。残り{" "}
+                    <strong>{sendLimit - countSent} 通</strong> 送信できます。
+                  </span>
+                ) : (
+                  <span>
+                    本日の送信リミットに達しましたが、引き続きテンプレート作成はご利用いただけます。
+                  </span>
+                )}
+              </div>
             </div>
           </div>
+
+          <MyTemplates expand={expand.template} setExpand={setExpand} />
+
+          <ContactList expand={expand.contact} setExpand={setExpand} />
+
+          <SentHistory
+            expand={expand.history}
+            setExpand={setExpand}
+            setCountSent={setCountSent}
+          />
+
+          <MarketingTool
+            expand={expand.marketingTool}
+            setExpand={setExpand}
+            analytics={analytics}
+            setAnalytics={setAnalytics}
+          />
         </div>
-
-        <MyTemplates expand={expand.template} setExpand={setExpand} />
-
-        <ContactList expand={expand.contact} setExpand={setExpand} />
-
-        <SentHistory
-          expand={expand.history}
-          setExpand={setExpand}
-          setCountSent={setCountSent}
-        />
-
-        <MarketingTool
-          expand={expand.marketingTool}
-          setExpand={setExpand}
-          analytics={analytics}
-          setAnalytics={setAnalytics}
-        />
-      </div>}
+      )}
     </div>
   );
 };
