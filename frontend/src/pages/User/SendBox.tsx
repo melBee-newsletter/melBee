53946c08f759498a5fba17c7231b90990f56209a
kugaitomomi <tottomo7@gmail.com>
2022-09-26 15:37:52 +0900

    chore change text

--- frontend/src/pages/User/SendBox.tsx
+++ frontend/src/pages/User/SendBox.tsx
@@ -6,7 +6,11 @@ import "./sendbox.css";
 import { contactAPI, emailAPI, sentHistoryAPI } from "./api";
 import { clickEvent, Props } from "../../type";
 
-const SendBox: React.FC<Props["send"]> = ({ analytics, reachLimit, setCountSent }) => {
+const SendBox: React.FC<Props["send"]> = ({
+  analytics,
+  reachLimit,
+  setCountSent,
+}) => {
   const navigate = useNavigate();
   const [subject, setSubject] = useState<string>("『melBee』からのお便り");
   const [allEmails, setAllEmails] = useState<string[]>([]);
@@ -110,7 +114,8 @@ const SendBox: React.FC<Props["send"]> = ({ analytics, reachLimit, setCountSent 
     };
     if (receivers.length > 0) {
       setLoading(true);
-      await emailAPI.send(emailBody)
+      await emailAPI
+        .send(emailBody)
         .then((sendComplete) => {
           setSendComplete(sendComplete);
           setLoading(!sendComplete);
@@ -156,96 +161,103 @@ const SendBox: React.FC<Props["send"]> = ({ analytics, reachLimit, setCountSent 
 
   return (
     <div>
-      {loading ? <Loading word={"S E N D I N G"} /> :
-      <div>
-      {sendComplete && (
-        <SendComplete
-          reachLimit={reachLimit}
-          setSendComplete={setSendComplete}
-        />
-      )}
-      {reachLimit && !sendComplete && (
-        <div className="flex justify-center">
-          <p>申し訳ございません、本日の送信リミットに達しました。</p>
-        </div>
-      )}
-      {!reachLimit && !sendComplete && (
-        <div className="sendArea pt-24 mx-auto mb-28">
-          <div className="mb-8">
-            <h2 className="text-xl font-bold mb-1">メールタイトルのご入力</h2>
-            <p className="mb-5">送信するメールのタイトルをご入力ください。</p>
-            <input
-              type="text"
-              placeholder="メールタイトルの入力をしてください。"
-              onChange={(e) => handleSubject(e.target.value)}
-              id="subjectId"
-              value={subject}
-              className="border rounded-lg p-2 text-lg sendTitle w-full"
+      {loading ? (
+        <Loading word={"S E N D I N G"} />
+      ) : (
+        <div>
+          {sendComplete && (
+            <SendComplete
+              reachLimit={reachLimit}
+              setSendComplete={setSendComplete}
             />
-          </div>
-          <h2 className="text-xl font-bold mb-1">メールアドレスの選択</h2>
-          <p className="mb-4">送信先メールアドレスを選択してください。</p>
-          <div className="flex text-xl pl-6">
-            <input
-              type="checkbox"
-              onChange={handleCheckAll}
-              disabled={!allEmails.length}
-            />
-            {!selectAll ? (
-              <p className="text-base ml-2">アドレスを全て選択</p>
-            ) : (
-              <p className="text-base ml-2">アドレスを全て解除</p>
-            )}
-          </div>
-          <div className="scroll flex flex-wrap border items-start rounded-xl mt-2 mb-4 px-3 py-1 bg-white">
-            {allEmails.map((email, i) => {
-              return displayEmailWithCheckbox(email, i);
-            })}
-          </div>
-          <div className="mb-8">
-            <div>
-              <p className="mb-2 text-left">
-                メールアドレスを新規登録したい方は、こちらからご入力いただけます。
-              </p>
-              <div className="text-left">
-                <form onSubmit={handleAdd}>
-                  <input
-                    className="border rounded-lg p-2 text-base w-3/6"
-                    type="email"
-                    value={email}
-                    placeholder="新規メールアドレスの登録はこちらから"
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                  <button className="rounded-xl px-6 py-2 drop-shadow-xl text-lg text-white font-medium bg-blueGradation ml-2 w-28">
-                    {" "}
-                    追加{" "}
-                  </button>
-                </form>
+          )}
+          {reachLimit && !sendComplete && (
+            <div className="flex justify-center">
+              <p>申し訳ございません、本日の送信リミットに達しました。</p>
+            </div>
+          )}
+          {!reachLimit && !sendComplete && (
+            <div className="sendArea pt-24 mx-auto mb-28">
+              <div className="mb-8">
+                <h2 className="text-xl font-bold mb-1">
+                  メールタイトルのご入力
+                </h2>
+                <p className="mb-5">
+                  送信するメールのタイトルをご入力ください。
+                </p>
+                <input
+                  type="text"
+                  placeholder="メールタイトルの入力をしてください。"
+                  onChange={(e) => handleSubject(e.target.value)}
+                  id="subjectId"
+                  value={subject}
+                  className="border rounded-lg p-2 text-lg sendTitle w-full"
+                />
+              </div>
+              <h2 className="text-xl font-bold mb-1">メールアドレスの選択</h2>
+              <p className="mb-4">送信先メールアドレスを選択してください。</p>
+              <div className="flex text-xl pl-6">
+                <input
+                  type="checkbox"
+                  onChange={handleCheckAll}
+                  disabled={!allEmails.length}
+                />
+                {!selectAll ? (
+                  <p className="text-base ml-2">アドレスを全て選択</p>
+                ) : (
+                  <p className="text-base ml-2">アドレスを全て解除</p>
+                )}
+              </div>
+              <div className="scroll flex flex-wrap border items-start rounded-xl mt-2 mb-4 px-3 py-1 bg-white">
+                {allEmails.map((email, i) => {
+                  return displayEmailWithCheckbox(email, i);
+                })}
+              </div>
+              <div className="mb-8">
+                <div>
+                  <p className="mb-2 text-left">
+                    メールアドレスを新規登録したい方は、こちらからご入力いただけます。
+                  </p>
+                  <div className="text-left">
+                    <form onSubmit={handleAdd}>
+                      <input
+                        className="border rounded-lg p-2 text-base w-3/6"
+                        type="email"
+                        value={email}
+                        placeholder="新規メールアドレスの登録はこちらから"
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                      <button className="rounded-xl px-6 py-2 drop-shadow-xl text-lg text-white font-medium bg-blueGradation ml-2 w-28">
+                        {" "}
+                        追加{" "}
+                      </button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex justify-center mt-8">
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    navigate("/user/preview");
+                  }}
+                  className="rounded-xl px-6 py-2 drop-shadow-xl text-lg text-white font-medium bg-grayGradation"
+                >
+                  &lt; プレビュー
+                </button>
+                <button
+                  type="submit"
+                  onClick={handleSend}
+                  className="rounded-xl px-6 py-2 drop-shadow-xl text-lg text-white font-medium bg-orangeGradation ml-6 w-28"
+                >
+                  送信
+                </button>
               </div>
             </div>
-          </div>
-
-          <div className="flex justify-center mt-8">
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                navigate("/user/preview");
-              }}
-              className="rounded-xl px-6 py-2 drop-shadow-xl text-lg text-white font-medium bg-grayGradation ml-3 w-28"
-            >
-              戻る
-            </button>
-            <button
-              type="submit"
-              onClick={handleSend}
-              className="rounded-xl px-6 py-2 drop-shadow-xl text-lg text-white font-medium bg-orangeGradation ml-3 w-28"
-            >
-              送信
-            </button>
-          </div>
+          )}
         </div>
       )}
-      </div>}
     </div>
   );
 };
