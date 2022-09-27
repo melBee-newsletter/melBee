bc78af1c265af599424e0e149b1dc0889ec9767b
kugaitomomi <tottomo7@gmail.com>
2022-09-27 10:03:30 +0900

    chore adjust design

--- frontend/src/pages/User/components/NotLoggedIn.tsx
+++ frontend/src/pages/User/components/NotLoggedIn.tsx
@@ -5,7 +5,7 @@ import { EmailForm } from "../../../components/Interfaces";
 import headerLogo from "../assets/logo.png";
 import Login from "../../../components/Login";
 import Signup from "../../../components/Signup";
-import { checkEmail } from "../../../api"
+import { checkEmail } from "../../../api";
 import { useTranslation } from "react-i18next";
 
 const NotLoggedIn: React.FC = () => {
@@ -27,45 +27,59 @@ const NotLoggedIn: React.FC = () => {
   return (
     <div className="h-screen justify-center pt-48">
       <div className="flex justify-center mb-6">
-        <img src={headerLogo} alt="melBee_logo" className="animate-pulse" width="200" />
+        <img
+          src={headerLogo}
+          alt="melBee_logo"
+          className="animate-pulse"
+          width="200"
+        />
       </div>
-      <h1 className="text-2xl mb-8">{t("melBeeはログインされた方のみご利用になれます。")}</h1>
-        <p className="text-xl mb-3 text-gray-500">
-          {t("ログインまたは無料で新規登録")}
-        </p>
-        <div className="flex justify-center items-center">
-            {!isEmailSubmitted && (
-              <div className="">
-                <form id="mailForm" onSubmit={handleSubmit} className="flex items-center items-center">
-                  <input
-                    type="mail"
-                    value={email}
-                    className="inputArea bg-gray-100 border-gray rounded lg:w-96"
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder="youremail@example.com"
-                    id="email_signup"
+      <h1 className="text-2xl mb-8">
+        {t("melBeeはログインされた方のみご利用になれます。")}
+      </h1>
+      <p className="text-xl mb-3 text-gray-500">
+        {t("ログインまたは無料で新規登録")}
+      </p>
+      <div className="flex justify-center items-center">
+        {!isEmailSubmitted && (
+          <div className="relative">
+            <form
+              id="mailForm"
+              onSubmit={handleSubmit}
+              className="flex items-center items-center"
+            >
+              <input
+                type="mail"
+                value={email}
+                className="inputArea bg-gray-100 border-gray rounded lg:w-96"
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="youremail@example.com"
+                id="email_signup"
+              />
+              {email ? (
+                <button className="">
+                  <FontAwesomeIcon
+                    icon={faArrowRight}
+                    className="bg-yellow-300 p-3 rounded-3xl text-white"
                   />
-                  {email ? <button 
-                    className="">
-                      <FontAwesomeIcon
-                        icon={faArrowRight}
-                        className="bg-yellow-300 p-3 rounded-3xl text-white"
-                      />
-                    </button> :
-                    <button 
-                      disabled={true}
-                      className="">
-                      <FontAwesomeIcon
-                        icon={faArrowRight}
-                        className="grayscale bg-yellow-300 p-3 rounded-3xl text-white"
-                      />
-                    </button>}
-                </form>
-              </div>
-            )}
+                </button>
+              ) : (
+                <button
+                  disabled={true}
+                  className="md:absolute md:top-1.5 right-[-16px]"
+                >
+                  <FontAwesomeIcon
+                    icon={faArrowRight}
+                    className="grayscale bg-yellow-300 p-3 rounded-3xl text-white"
+                  />
+                </button>
+              )}
+            </form>
           </div>
-          {isUserSignnedUP && isEmailSubmitted && <Login email={email} />}
-          {isEmailSubmitted && !isUserSignnedUP && <Signup email={email} />}
+        )}
+      </div>
+      {isUserSignnedUP && isEmailSubmitted && <Login email={email} />}
+      {isEmailSubmitted && !isUserSignnedUP && <Signup email={email} />}
     </div>
   );
 };
